ce629624bbd5e5168c81be8bfe3fed53d0f424d1
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-08-18 15:07:38 +0100

    add basic page structure for search page
    connects index.html with script.js & style.css
    
    makes sure the submit button is being selected by script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const submitButton = document.getElementById('submit-acronym');
+const acronymResult = document.getElementById('acronym-result');
+
+submitButton.addEventListener('click', searchAcronym);
+
+function searchAcronym() {
+    console.log('button was clicked');
+};
