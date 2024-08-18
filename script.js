dde41ead85dcad2f5783f333ae6d90837c18a979
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-08-18 15:33:07 +0100

    stop the form reloading the page when the search button is clicked
    adds the acronym variable, capture user's input live and pass it to acronym

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const submitButton = document.getElementById('submit-acronym');
+const acronymInput = document.getElementById('acronym-input');
 const acronymResult = document.getElementById('acronym-result');
+const acronymSearchForm = document.getElementById('acronym-search-form');
+let acronym = '';
+
+// stop the form reloading the page when the submit button is clicked
+acronymSearchForm.addEventListener('submit', (e) => e.preventDefault());
 
 submitButton.addEventListener('click', searchAcronym);
 
+// capturing user's input "live", might help with doing the autocomplete
+// stretch goal!
+acronymInput.addEventListener('input', function (e) {
+    // passing the user's input to the acronym variable
+    acronym += e.data;
+    console.log(e.data)
+})
+
 function searchAcronym() {
-    console.log('button was clicked');
+    console.log('this is acronym', acronym);
 };
