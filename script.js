f955ece61d20f5e6e9f69d453489c4c0905d7cc4
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-08-20 11:53:22 +0100

    refactors the search so it's now case insensitive

--- script.js
+++ script.js
@@ -20,21 +20,55 @@ acronymInput.addEventListener('input', function (e) {
 
 async function searchAcronym() {
     console.log('this is acronym', acronym);
-    // connect to db.json
     try {
+        // connect to db.json
         const res = await fetch('./db.json');
         const data = await res.json();
         // confirm I'm actually connecting with db.json!
         console.table(data);
-        if (data[acronym]) {
-            // it works
-            console.log('this is the acronym: ', data[acronym]);
-        } else {
-            console.log(`this didn't work`)
+
+        // need to make search case insensitive!
+        acronym = acronym.toLowerCase();
+        console.log('lowercase acro ', acronym);
+
+        // the if...else was returning every result so using match to act as first validation
+        let match = false;
+        // org needs to hold the value of a matched result so it can be passed to the user
+        let org = '';
+        // just to make the user message nicer :)
+        let correctAcronym = ''
+
+        for (let key in data) {
+            if (key.toLowerCase() === acronym) {
+                // it works!
+                console.log('this is the acronym: ', data[key]);
+                match = true;
+                org = data[key];
+                correctAcronym = key;
+            } 
+            
+        if (match) {
+            acronymResult.textContent = `The organisation name with the acronym ${correctAcronym} is ${org}.`;
         }
+            else {
+                acronymResult.textContent = `There is no organisation matching ${correctAcronym}, please try again.`;
+                }
+        }        
     } catch (error) {
         console.error('There was an error fetching the data:', error);
     }
     // need to clear acronym before trying another search
     acronym = '';
 };
+
+// guff
+// this is not case insensitive...
+// if (data[acronym]) {
+//     // it works
+//     console.log('this is the acronym: ', data[acronym]);
+//     acronymResult.textContent = `The organisation name that matches
+//     this acronym is ${data[acronym]}.`;
+// } else {
+//     acronymResult.textContent = 'There is no organisation matching that acronym, please try again.'
+//     console.log(`this didn't work`)
+// }
