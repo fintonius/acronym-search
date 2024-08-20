db9e5740dd78a53d7dacca5540df54b4d961b1ab
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-08-20 10:23:38 +0100

    adds functionality to search the database for a submitted acronym and return the results

--- script.js
+++ script.js
@@ -11,21 +11,30 @@ submitButton.addEventListener('click', searchAcronym);
 
 // capturing user's input "live", might help with doing the autocomplete stretch goal?
 acronymInput.addEventListener('input', function (e) {
+    console.log(e.data);
     // passing the user's input to the acronym variable
-    // this isn't working with saved autofill text! Maybe just disable autofill
+    // this isn't working with saved autocomplete text! Maybe just disable autocomplete
     // for now and try to solve it later?!
     acronym += e.data;
-    console.log(e.data)
 })
 
 async function searchAcronym() {
     console.log('this is acronym', acronym);
+    // connect to db.json
     try {
         const res = await fetch('./db.json');
         const data = await res.json();
         // confirm I'm actually connecting with db.json!
         console.table(data);
+        if (data[acronym]) {
+            // it works
+            console.log('this is the acronym: ', data[acronym]);
+        } else {
+            console.log(`this didn't work`)
+        }
     } catch (error) {
         console.error('There was an error fetching the data:', error);
     }
+    // need to clear acronym before trying another search
+    acronym = '';
 };
